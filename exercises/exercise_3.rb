f126ec43b568d493f243bfd2de8c55892146b568
Karamvir-Bains <kbains2000@gmail.com>
2023-03-08 18:20:27 -0700

    Delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,13 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+class Store < ActiveRecord::Base
+end
+
+@store3 = Store.find(3)
+
+puts "Number of stores: #{Store.count}"
+
+@store3.destroy
+
+puts "Number of stores: #{Store.count}"
