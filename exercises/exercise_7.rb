a589cbd37911da2b33a58cb6a1f23efadc4345e7
Karamvir-Bains <kbains2000@gmail.com>
2023-03-08 20:28:06 -0700

    Validations for both models

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,40 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: { only_integer: true, greater_than_or_equal_to: 40, less_than_or_equal_to: 200 }
+  validates :store, presence: true
+end
+
+
+class Store < ActiveRecord::Base
+  has_many :employees
+
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  validate :must_carry_men_or_women_apparel
+
+  def must_carry_men_or_women_apparel
+    if !mens_apparel.present? && !womens_apparel.present?
+      errors.add(:mens_apparel, "Store must carry at least one of the men's or women's apparel")
+      errors.add(:womens_apparel, "Store must carry at least one of the men's or women's apparel")
+    end
+  end
+end
+
+puts "Enter a store name:"
+store_name = gets.chomp
+
+store = Store.new(name: store_name)
+store.save
+
+if store.errors.any?
+  store.errors.full_messages.each do |message|
+    puts message
+  end
+end
